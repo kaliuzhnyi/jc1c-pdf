140b747e692c73f928d0d8202b81e0c373e3031a
Ivan Kaliuzhnyi <kalyuzhny.ivan@gmail.com>
2021-10-04 09:24:01 +0300

    Update dependencies - jc1c(1.0.7)

--- src/main/java/org/jc1c/pdf/App.java
+++ src/main/java/org/jc1c/pdf/App.java
@@ -7,22 +7,8 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException {
 
-        String hostname = (args.length >= 1 && !args[0].isBlank())
-                ? args[0]
-                : null;
-
-        Integer port = (args.length >= 2 && !args[1].isBlank())
-                ? Integer.valueOf(args[1])
-                : null;
-
-        String apiKey = (args.length >= 3 && !args[2].isBlank())
-                ? args[2]
-                : null;
-
         JServer.builder()
-                .withHostname(hostname)
-                .withPort(port)
-                .withApiKey(apiKey)
+                .withArgs(args)
                 .withBacklog(3)
                 .withThreadPool(3)
                 .withHandlersController(ConverterHandlers.class)
